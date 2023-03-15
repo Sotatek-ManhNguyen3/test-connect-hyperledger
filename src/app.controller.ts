c39d32812a0763f87c6cbecb1fbd963fa021140a
Tien Manh <manh.nguyen3@sotatek.com>
2023-03-15 11:26:00 +0700

    Call successfully to example smartcontract of fabric

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async getHello(): Promise<string> {
+    return await this.appService.getHello();
+  }
+
+  @Get('/all-assets')
+  async getAllAssets() {
+    return await this.appService.getAllAssets();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CONTRACT_ACTIONS, fullProcess } from "./utils/gateway";
+
+@Injectable()
+export class AppService {
+  async getHello(): Promise<string> {
+    await this.getAllAssets();
+    return 'Hello World!';
+  }
+
+  async getAllAssets() {
+    return await fullProcess(CONTRACT_ACTIONS.GET_ALL_ASSETS);
+  }
+}
